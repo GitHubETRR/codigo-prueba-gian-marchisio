09a43e3e8d93bac93ebad4e4fea159a50da15c76
gian-marchisio <gmarchisio@etrr.edu.ar>
2024-05-31 14:43:26 -0300

    Update ACT 21 - Convesor de fecha por DDMMAAAA.c

--- ACT 21 - Convesor de fecha por DDMMAAAA.c
+++ ACT 21 - Convesor de fecha por DDMMAAAA.c
@@ -6,11 +6,11 @@
 
 
 int main(){
-   unsigned int fecha, dia, mes, año;
+   unsigned int fecha, dia, mes, año, bisiestos;
    
    printf("Ingrese el valor de la fecha en formato DDMMAAAA:\n");
    scanf("%d", &fecha);
-
+   
    dia = fecha / 1000000;
    fecha = fecha % 1000000;
 
@@ -18,8 +18,23 @@ int main(){
    fecha = fecha % 10000;
 
    año = fecha;
-
-   printf("Dia: %d, Mes: %d, Año: %d\n", dia, mes, año);
-
+   
+   if(dia > 30 && mes == 2){
+       printf("Fecha invalida, febrero solo tiene 28 dias en años no bisiestos");
+   }else if(dia == 29 && mes == 2){
+       bisiestos = año % 4;
+       if(bisiestos == 0){
+           printf("Dia: %d, Mes: %d, Año: %d\n", dia, mes, año);
+       }else if(bisiestos != 0){
+           printf("Este año no es bisiesto, por lo que febrero no tiene 29 dias");
+       }
+   }else if(dia > 31 && (mes == 2 || mes == 4 || mes == 6 || mes == 9 || mes == 11)){
+       printf("Fecha invalida, ninguno de estos meses tiene 31 dias");
+   }else if(mes > 12 || dia > 32){
+       printf("Fecha invalida, el mes no puede ser mayor a 13 ni el dia mayor a 32");
+   }else{
+       printf("Dia: %d, Mes: %d, Año: %d\n", dia, mes, año);
+   }
    return 0;
-}
+  }
+   
